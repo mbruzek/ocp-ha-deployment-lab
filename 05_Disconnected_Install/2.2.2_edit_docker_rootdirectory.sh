20ee80016e263bee3d5a8988a8fd95fbf0429d04
Matt Bruzek <mbruzek@gmail.com>
2018-08-02 10:35:48 -0500

    More docker commands.

--- 05_Disconnected_Install/2.2.2_edit_docker_rootdirectory.sh
+++ 05_Disconnected_Install/2.2.2_edit_docker_rootdirectory.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 set -x
-echo "Edit /etc/docker-distribution/registry/config.yml "
+echo "2.2.2 Edit /etc/docker-distribution/registry/config.yml "
 sed -i 's/^.*rootdirectory.*$/        rootdirectory: \/srv\/repohost\/registry/' /etc/docker-distribution/registry/config.yml
+echo "2.2.3 Enable logging to journalctl"
+cat << EOF >> /etc/docker-distribution/registry/config.yml
+log:
+  accesslog:
+    disabled: false
+  level: info
+  formatter: text
+  fields:
+    service: registry
+    environment: staging
+EOF
+echo "2.2.4 Create directory to hold images."
+mkdir -p /srv/repohost/registry
+systemctl enable docker-distribution
+systemctl start docker-distribution
+systemctl status docker-distribution
